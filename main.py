e10b9d23a3d2fa8420e3bcc80512c729a411f798
carter <carter369@live.co.uk>
2024-11-27 20:41:24 +0000

    added part3

--- main.py
+++ main.py
@@ -10,7 +10,10 @@ import shutil
 
 
 class DataPrep:
-    def __init__(self):
+    def __init__(self, use_cache=False):
+        self.use_cache = use_cache
+        self.project_root = Path(__file__).parent
+
         self.connection_config = self.get_config('config.yaml')
         self.datatype_config = self.get_config('data_types.yaml')
 
@@ -44,9 +47,16 @@ class DataPrep:
         return config
     
     def get_all_data_table(self, table_name):
+        cache_location = self.project_root.joinpath('cache', table_name + '_data.csv')
+        if self.use_cache:
+            if os.path.exists(self.project_root.joinpath(cache_location)):
+                return pd.read_csv(cache_location)
+            
         with self.engine.connect() as connection:
             result = connection.execute(text(f"SELECT * FROM {table_name}"))
             df = pd.DataFrame(result.fetchall(), columns=result.keys())
+
+        df.to_csv(cache_location, index=False)
         return df 
     
     def clean_data(self, df, table_name):
@@ -69,47 +79,85 @@ class TransformSave:
         self.root_dir = Path(__file__).parent
         self.output_dir = self.root_dir.joinpath('outputs')
         self.prepare_output_dir()
-        self.perform_tasks(customers_df, sales_df)
+        self.perform_part2_tasks(customers_df.copy(), sales_df.copy()) # Using a copy to gurantee no changes in the cleaned data
+        self.perform_part3_tasks(customers_df, sales_df)
 
     def prepare_output_dir(self):
         if os.path.exists(self.output_dir):
             shutil.rmtree(self.output_dir)
         os.makedirs(self.output_dir)
             
-    def perform_tasks(self, customers_df, sales_df):
+    def save_task(self, df, task_name, subfolder):
+        path_to_subfolder = self.output_dir.joinpath(subfolder)
+        if not os.path.exists(path_to_subfolder):
+            os.makedirs(path_to_subfolder)
+        df.to_csv(self.output_dir.joinpath(subfolder, task_name + '.csv'), index=False)
 
-        print('There are', len(customers_df['customer_id'].unique()), 'unique customers')
 
-        sorted_customers = customers_df.sort_values(['age'], ascending=True)
-        sorted_customers.to_csv(
-            self.output_dir.joinpath('sorted_customers.csv'), index=False
+    def perform_part2_tasks(self, customers_df, sales_df, subfolder='part_2'):
+        # Task 1
+        number_of_cutomers =  len(customers_df['customer_id'].unique())
+        print('There are', number_of_cutomers, 'unique customers')
+        self.save_task(
+            pd.DataFrame({'There Are This many unique customers': [number_of_cutomers]}),
+            '1_number_unique_customers',
+            subfolder
             )
+       
+       # Task 2
+        sorted_customers = customers_df.sort_values(['age'], ascending=True)
         
         print(sorted_customers)
+        self.save_task(sorted_customers, '2_sorted_customer', subfolder)
         
-        sum_sales = sales_df.groupby('customer_id', as_index=False).agg(
-            total_sale_amount=('sale_amount', 'sum')
-            )
-        
-        max_row = sum_sales.loc[sum_sales['total_sale_amount'].idxmax(), :]
-        max_customer = customers_df.loc[
-            customers_df['customer_id'] == max_row['customer_id']
-            ].reset_index(drop=True)
-        
-        max_customer.to_csv(self.output_dir.joinpath('max_customer.csv'), index=False)
-        print(max_customer)
+        # Task 3
+        spend_by_customer = self.get_total_spend_by_customer(customers_df, sales_df)
+        max_customer_row = pd.DataFrame([spend_by_customer.loc[spend_by_customer['total_spend'].idxmax(), :]]).reset_index(drop=True)
+        max_customer_row.columns = list(customers_df.columns) + ['total_spend'] 
 
-        sum_sales_product = sales_df.groupby('product_category', as_index=False).agg(
-            total_sale_amount=('sale_amount', 'sum')
-            )
-        max_row = sum_sales_product.loc[sum_sales_product['total_sale_amount'].idxmax(), :]
-        max_row.to_csv(self.output_dir.joinpath('max_product.csv'), index=False)
+        self.save_task(spend_by_customer, 'customer_spend', 'part3')
+        self.save_task(max_customer_row, '3_max_customer', subfolder)
+        print(max_customer_row)
+
+        # Task 4
+        sum_sales_product = self.get_sales_per_category(sales_df)
+        max_row = sum_sales_product.loc[sum_sales_product['total_spend'].idxmax(), :]
+        self.save_task(sum_sales_product, 'sales_per_category', 'part3')
+        self.save_task(max_row, '4_max_product', subfolder)
         print(max_row)
         
+        # Task 5
         transactions_by_date = sales_df.sort_values('sale_date', ascending=False)
-        transactions_by_date.to_csv(self.output_dir.joinpath('transactions_by_date.csv'), index=False)
+        self.save_task(transactions_by_date, '5_transactions_by_date', subfolder)
+        self.save_task(transactions_by_date, 'transactions_sorted_by_date', 'part3')
+
         print(transactions_by_date)
 
+    def get_total_spend_by_customer(self, customers_df, sales_df):
+        spend_by_customer = customers_df.merge(
+            sales_df, how='left', on='customer_id'
+            ).groupby(
+                ['customer_id', 'customer_name', 'age', 'region'],
+                as_index=False
+                ).agg(total_spend=('sale_amount', 'sum'))
+        
+        spend_by_customer['total_spend'] = spend_by_customer['total_spend'].round(2)
+
+        if spend_by_customer.shape[0] != len(customers_df['customer_id'].unique()):
+            raise ValueError('Number of customer_ids and customer_names are not equal'
+                             'There may be a misspelling in the customer_name.')
+        
+        return spend_by_customer
+    
+    def get_sales_per_category(self, sales_df):
+        sales_per_category = sales_df.groupby('product_category', as_index=False).agg(total_spend=('sale_amount','sum'))
+        sales_per_category['total_spend'] = sales_per_category['total_spend'].round(2)
+        return sales_per_category
+    
+    def perform_part3_tasks(self, customers_df, sales_df, subfolder='part3'):
+        self.save_task(customers_df, 'cleaned_customers', subfolder)
+        self.save_task(sales_df, 'cleaned_sales.csv', subfolder)
+        
 if __name__ == '__main__':
-    loaded_data = DataPrep()
+    loaded_data = DataPrep(use_cache=True)
     TransformSave(customers_df=loaded_data.customers_df, sales_df=loaded_data.sales_df)
